c7738a6c8814d38df9b898de32a26d1bdf6e05c4
arjun.dhingra2@mindtree.com <M1050760@mindtree.com>
2019-08-26 18:15:43 +0530

    Updated Search Controller

--- restaurant-search-service/src/main/java/com/restaurant/search/controller/SearchController.java
+++ restaurant-search-service/src/main/java/com/restaurant/search/controller/SearchController.java
@@ -3,11 +3,9 @@ package com.restaurant.search.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurant.search.entity.Restaurant;
@@ -22,87 +20,62 @@ public class SearchController {
 	@Autowired
 	private SearchService service;
 
-	/*
-	 * @PostMapping("/getRestaurants") public List<Restaurant>
-	 * getRestaurants(@RequestBody Restaurant restaurant) { return
-	 * service.getRestaurants(restaurant); }
-	 */
-
-	@Cacheable(value="location")
-	@GetMapping("/location/{location}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByLocation(@PathVariable String location, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/location/{location}/{pageindex}/{size}")
+	public List<Restaurant> searchByLocation(@PathVariable String location, @PathVariable int page,
+			@PathVariable int size) throws RestaurantNotFoundException {
 		try {
-			return service.searchByLocation(location, startIndex, endIndex);
+			return service.searchByLocation(location, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant" + e);
 		}
 	}
 
-	/*
-	 * @GetMapping("/budget/{budget}/{startIndex}/{endIndex}") public
-	 * Map<String,Object> searchByBudget(@PathVariable double budget, @PathVariable
-	 * int startIndex,
-	 * 
-	 * @PathVariable int endIndex) throws RestaurantNotFoundException {
-	 * List<Restaurant> restaurants = null; Map<String, Object> responses = new
-	 * HashMap<String, Object>(); try { restaurants =
-	 * service.searchByBudget(budget,startIndex,endIndex); responses.put("SUCCESS",
-	 * "true"); responses.put("BODY", restaurants); responses.put("ERROR", "false");
-	 * responses.put("HttpStatus", HttpStatus.OK.value());
-	 * responses.put("HttpStatusCode",HttpStatus.OK); } catch (ServiceException e) {
-	 * responses.put("Message", "Restaurant Not Found"); responses.put("HttpStatus",
-	 * "Error Occured"); responses.put("HttpStatusCode", HttpStatus.BAD_REQUEST);
-	 * responses.put("SUCCESS", "false"); responses.put("ERROR", "true"); } return
-	 * responses; }
-	 */
-
-	@GetMapping("/distance/{distance}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByDistance(@PathVariable String distance, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/distance/{distance}/{pageindex}/{size}")
+	public List<Restaurant> searchByDistance(@PathVariable String distance, @PathVariable int page,
+			@PathVariable int size) throws RestaurantNotFoundException {
 		try {
-			return service.searchByDistance(distance, startIndex, endIndex);
+			return service.searchByDistance(distance, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant");
 		}
 	}
 
-	@GetMapping("/cuisine/{cuisine}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByCuisine(@PathVariable String cuisine, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/cuisine/{cuisine}/{pageindex}/{size}")
+	public List<Restaurant> searchByCuisine(@PathVariable String cuisine, @PathVariable int page,
+			@PathVariable int size) throws RestaurantNotFoundException {
 		try {
-			return service.searchByCuisine(cuisine, startIndex, endIndex);
+			return service.searchByCuisine(cuisine, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant");
 		}
 	}
 
-	@GetMapping("/name/{name}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByName(@PathVariable String name, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/name/{name}/{pageindex}/{size}")
+	public List<Restaurant> searchByName(@PathVariable String name, @PathVariable int page, @PathVariable int size)
+			throws RestaurantNotFoundException {
 		try {
-			return service.searchByName(name, startIndex, endIndex);
+			return service.searchByName(name, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant");
 		}
 	}
 
-	@GetMapping("/budget/{budget}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByBudget(@PathVariable double budget, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/budget/{budget}/{pageindex}/{size}")
+	public List<Restaurant> searchByBudget(@PathVariable double budget, @PathVariable int page, @PathVariable int size)
+			throws RestaurantNotFoundException {
 		try {
-			return service.searchByBudget(budget, startIndex, endIndex);
+			return service.searchByBudget(budget, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant");
 		}
 
 	}
 
-	@GetMapping("/ratings/{ratings}/{startIndex}/{endIndex}")
-	public List<Restaurant> searchByRatings(@PathVariable double ratings, @PathVariable int startIndex,
-			@PathVariable int endIndex) throws RestaurantNotFoundException {
+	@GetMapping("/ratings/{ratings}/{page}/{size}")
+	public List<Restaurant> searchByRatings(@PathVariable double ratings, @PathVariable int page,
+			@PathVariable int size) throws RestaurantNotFoundException {
 		try {
-			return service.searchByRatings(ratings, startIndex, endIndex);
+			return service.searchByRatings(ratings, page, size);
 		} catch (ServiceException e) {
 			throw new RestaurantNotFoundException("Unable to Search Restaurant");
 		}
